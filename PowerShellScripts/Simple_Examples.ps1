e121bc056ab1cd4a9d53875c29500c99c15a5050
Ross Thompson <ross.thompson@outlook.com>
2017-01-28 16:43:08 -0500

    Added some chapter 1 scripts

---
+++ PowerShellScripts/Simple_Examples.ps1
@@ -0,0 +1,10 @@
+﻿# Example commands
+
+dir | sort -Property LastWriteTime -Descending | Select-Object -First 10 |  ForEach-Object {$total += $_.length }
+Get-Process | sort -Descending ws | select -First 3
+
+$i = 0
+while ($i++ -lt 10) { if ($i % 2) {"$i is odd"}}
+
+foreach ($i in 1..10) { if ($i % 2) {"$i is odd"}}
+
